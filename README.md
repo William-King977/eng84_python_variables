ff4c8d476c63a6a25da8244192116c59b7b7d74a
William King <williamking9772@gmail.com>
2021-03-22 16:20:49 +0000

    expanded examples on variables.py

--- README.md
+++ README.md
@@ -10,7 +10,12 @@
 print("hello world")
 ```
 
-- if the outcome is ```hello world``` then it is working fine
+- if the outcome is ```hello world```, then it is working fine
 
 ### .gitignore to ignore files/folders that we do not want to send to GitHub
 - create file called ```.gitignore```
+
+
+### .gitignore with python variables and README.md pushed to GitHub
+
+- To add a comment in a .py file, we use a ```#```
--- variables.py
+++ variables.py
@@ -1,1 +1,20 @@
-print("Hello world")
+# pseudo coding
+# Testing the env with hello world
+# print("Hello world")
+
+# What is a variable?
+course_name = "DevOps 84" # string
+student_name = "William" # string
+age = 18 # integer
+hourly_wage = 8.75 # float
+
+# print(student_name)
+# print(hourly_wage)
+# print(age)
+
+# To check the type of data, use type()
+print(type(age))
+
+# overwriting a variable
+student_name = "Zydrunas"
+print(student_name)
